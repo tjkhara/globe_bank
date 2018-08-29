506efedac04da22ed03e3ea56661b424b2c3ddc3
Tajeshwar Khara <tkhara@gmail.com>
2018-08-29 18:33:51 +0530

    Initial commit. Finished chapter 1.

---
+++ private/shared/staff_header.php
@@ -0,0 +1,23 @@
+<?php
+    if(!isset($page_title))
+    {
+        $page_title = "Staff Area";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GBI - <?php echo $page_title; ?></title>
+    <link rel="stylesheet" href="../stylesheets/staff.css" type="text/css" media="all">
+</head>
+<body>
+<header>
+    <h1>GBI Staff Area</h1>
+</header>
+
+<navigation>
+    <ul>
+        <li><a href="index.php">Menu</a></li>
+    </ul>
+</navigation>
